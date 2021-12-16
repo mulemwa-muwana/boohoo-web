9a952db3f3a359ff37e98a99362723adcb50886b
Jordan Benyon <jordan.benyon@boohoo.com>
2021-12-16 13:00:36 +0000

    initial framework push

---
+++ cypress/integration/0-web-automation/login.spec.ts
@@ -0,0 +1,13 @@
+describe('Core front-end website', () => {
+
+  // This will execute before every single test, we're just going to the baseURL.
+  beforeEach(() => {
+    cy.visit('/');
+  })
+
+  // This is a single test.
+  it('can log in a user', () => {
+    // Test steps here...
+  })
+
+})

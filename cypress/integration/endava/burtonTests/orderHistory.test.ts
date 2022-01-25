2da1b01cdbd88d700bbd5f47335860e94f18da59
Jordan Benyon <jordan.benyon@boohoo.com>
2022-01-25 12:55:42 +0000

    commented more code to restructure

--- cypress/integration/endava/burtonTests/orderHistory.test.ts
+++ cypress/integration/endava/burtonTests/orderHistory.test.ts
@@ -1,22 +1,19 @@
-import { AccountPage } from "../../page-objects/accountPage"
-import { HomePage } from "../../page-objects/homePage"
-import { validatePageTitle, validateUrl, validatePageContent } from "../../page-objects/commonFunctions"
-import { ReturnsPage } from "../../page-objects/returnsPage"
-
-
 describe('My Account - order information', () => {
-
-    const homePage = new HomePage()
-    const accPage = new AccountPage()
-    const returnsPage = new ReturnsPage()
-
+    
     beforeEach(() => {
+        /*
         homePage.navigate()
-        homePage.acceptCookies()
         homePage.login(Cypress.env('validUsername'), Cypress.env('validPassword'))
+        */
+
+        /*
+        Home Page Navigate
+        Login Valid Credentials
+        */
     })
 
     it('Should be able to view order and start return', () => {
+        /*
         accPage.goToOrderHistory()
         validatePageTitle('Order History')
         accPage.openNewestOrder()
@@ -24,6 +21,17 @@ describe('My Account - order information', () => {
         accPage.startReturn()
         validateUrl('delivery-and-returns')
         validatePageContent(returnsPage.reuturnsAndRefundsTitle(), 'Returns & Refunds')
+        */
+
+        /*
+        Account Page Go To Order History
+        Validate on the correct page
+        Account Page, Open Latest Order
+        Validate Page Title
+        Account Page, start a return
+        Validate URL is correct
+        Validate Page Content says Returns and Refunds
+        */
     })
 })
 
--- cypress/integration/endava/burtonTests/refinmentsColor.test.ts
+++ cypress/integration/endava/burtonTests/refinmentsColor.test.ts
@@ -1,23 +1,29 @@
-import { TwoPieceSuitsPage } from "../../page-objects/twoPieceSuitsPage"
-import { HomePage } from "../../page-objects/homePage"
-import { validateUrl } from "../../page-objects/commonFunctions"
-
-
 describe('Filtering color', () => {
-
-    const homePage = new HomePage()
-    const twoPieceSuitsPage = new TwoPieceSuitsPage()
-
+    
     before(() => {
+        /*
         homePage.navigate()
-        homePage.acceptCookies()
+        */
+        
+        /*
+        HomePage navigate
+        */
     })
 
     it('Should preview only white boots', () => {
+        /*
         homePage.goTo2SuitsPage()
         validateUrl('2-piece-suits')
         twoPieceSuitsPage.selectColor('Blue')
         twoPieceSuitsPage.validateColor('Blue')
+        */
+       
+        /*
+        Home Page go to 2 suits page.
+        Validate URL is correct
+        Select Blue colour
+        Validate Colour is correctly selected
+        */
     })
 })
 

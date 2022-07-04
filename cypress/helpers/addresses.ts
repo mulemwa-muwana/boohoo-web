73307d1aaef8d558599dea67050414dfba01d1fc
Jordan Benyon <jordan.benyon@boohoo.com>
2022-07-04 15:42:14 +0100

    bnuyiasdrbfnuasdnf

--- cypress/helpers/addresses.ts
+++ cypress/helpers/addresses.ts
@@ -35,34 +35,6 @@ class Addresses {
     return addresses[locale];
   }
 
-  AddressLineUK: AddressData = {
-    firstName: 'Test',
-    lastName: 'Test',
-    phone: '+44 843 837 0041',
-    addrline1: '49-51 Dale St',
-    addrline2: '',
-    postcode: 'M1 2HG',
-    addressName: 'Boohoo',
-    country: 'United Kingdom',
-    city: 'Manchester',
-    countryCode: 'UK',
-    county: 'London',
-  };
-
-  AddressLineUK1: AddressData = {
-    firstName: 'Test',
-    lastName: 'Test',
-    phone: '+44 843 837 0041',
-    addrline1: 'Unit 1162',
-    addrline2: 'Westfield Shopping Centre, White City',
-    postcode: 'W12 7GD',
-    addressName: 'London Addr',
-    country: 'United Kingdom',
-    city: 'London',
-    countryCode: 'UK',
-    county: 'London',
-  };
-
   AddressLineUK2: AddressData = {
     firstName: 'New',
     lastName: 'Test',

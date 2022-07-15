b4416bc48f333df3b935435ebf52afa089e15299
miona.jovicevic <mionaboohoo@gmail.com>
2022-07-15 14:46:39 +0200

    product variants translations

--- cypress/helpers/skuAssertions.ts
+++ cypress/helpers/skuAssertions.ts
@@ -17,16 +17,16 @@ class skuAssertions {
 
   mainSkuColor: TranslationMap = {
     EN: 'blue',
-    FR: '',
-    NL: '',
-    DE: '',
-    DK: '',
-    FI: '',
-    NO: '',
-    SE: '',
-    IL: '',
-    IT: '',
-    ES: ''
+    FR: 'bleue',
+    NL: 'blauw',
+    DE: 'blau',
+    DK: 'blå',
+    FI: 'sininen',
+    NO: 'blå',
+    SE: 'blå',
+    IL: 'כָּחוֹל',
+    IT: 'blu',
+    ES: 'azul'
   };
 }
 

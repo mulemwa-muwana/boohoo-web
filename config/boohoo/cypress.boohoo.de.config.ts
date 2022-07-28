e85966afc300c9fddaecafbfb2f4a1d4914ed535
Ivan Vukovic <ivan.vukovic@endava.com>
2022-07-28 09:28:26 +0200

    Update configuration files format for new version of Cypress.

---
+++ config/boohoo/cypress.boohoo.de.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  projectId: "i6d3n8",
+
+  env: {
+    url: "https://storefront:Oreo2022@de-dwstg.boohoo.com/",
+    sku: "#DZZ79497",
+    brand: "boohoo.com",
+    locale: "DE",
+    language: "DE",
+  },
+
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  defaultCommandTimeout: 10000,
+  chromeWebSecurity: false,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
---
+++ config/boohoo/cypress.boohoo.es.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  projectId: "i6d3n8",
+
+  env: {
+    url: "https://storefront:Oreo2022@es-dwstg.boohoo.com/",
+    sku: "#DZZ79497",
+    brand: "boohoo.com",
+    locale: "ES",
+    language: "ES",
+  },
+
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  defaultCommandTimeout: 10000,
+  chromeWebSecurity: false,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});

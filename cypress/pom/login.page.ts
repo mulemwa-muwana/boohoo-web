64fe39886a8408947417d6f0da3f9174f6bb587f
tamara.cvetkovic <tamara.cvetkovic@boohoo.com>
2022-08-01 10:00:36 +0200

    updated loginPage

--- cypress/pom/login.page.ts
+++ cypress/pom/login.page.ts
@@ -1,4 +1,4 @@
-import { GroupBrands, SelectorBrandMap } from '../support/types';
+
 import AbstractPage from './abstract/abstract.page';
 import homePage from './home.page';
 
@@ -41,6 +41,8 @@ const selectors: SelectorBrandMap = {
   'misspap.com': undefined
 };
 
+const variables = Cypress.env() as EnvironmentVariables;
+
 class LoginPage implements AbstractPage {
 
   goto (): void {
@@ -56,13 +58,13 @@ class LoginPage implements AbstractPage {
 
   actions = {
     login (user: string, pass: string, opts = { force: false }) {
-      const loginIcon = selectors[GroupBrands.Boohoo].loginIcon;
+      const loginIcon = selectors[variables.brand].loginIcon;
       cy.get(loginIcon).click({ force: opts.force });
-      const loginEmail = selectors[GroupBrands.Boohoo].loginEmail;
+      const loginEmail = selectors[variables.brand].loginEmail;
       cy.get(loginEmail).type(user); 
-      const loginPassword = selectors[GroupBrands.Boohoo].loginPassword;
+      const loginPassword = selectors[variables.brand].loginPassword;
       cy.get(loginPassword).type(pass);
-      const loginButton = selectors[GroupBrands.Boohoo].loginButton;
+      const loginButton = selectors[variables.brand].loginButton;
       cy.get(loginButton).click();
     }
   };

17d1e351f0e4dc473b9ae6de2bcdda9e08169429
tamara.cvetkovic <tamara.cvetkovic@boohoo.com>
2022-08-30 10:27:20 +0200

    german language changes

--- cypress/e2e/criticalPath/homePage.cy.ts
+++ cypress/e2e/criticalPath/homePage.cy.ts
@@ -275,7 +275,7 @@ describe('Footer verification', () => {
     const variables = Cypress.env() as EnvironmentVariables;
     GlobalFooter.actions.checkFooterLinkByText(assertionText.termsAndCond[variables.language]); 
   });
-  it('Verify that Footer Navigation Component is present and Links are functional - Privacy Notice - Updated March 2021', () => {
+  it('Verify that Footer Navigation Component is present and Links are functional - Privacy - Updated March 2021', () => {
     const variables = Cypress.env() as EnvironmentVariables;
     GlobalFooter.actions.checkFooterLinkByText(assertionText.privacyPolicy[variables.language]);
   });

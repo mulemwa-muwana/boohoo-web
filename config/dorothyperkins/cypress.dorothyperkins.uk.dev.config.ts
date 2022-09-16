ca86ca1b81d3922cd1a2af30b66278afb55398e7
Jordan Benyon <jordan.benyon@boohoo.com>
2022-09-16 10:14:20 +0100

    push to master oops

---
+++ config/dorothyperkins/cypress.dorothyperkins.uk.dev.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  projectId: 'i6d3n8',
+
+  env: {
+    url: 'https://storefront:Oreo2022@dwdev.dorothyperkins.com/',
+    sku: '#AQQ42330-141',
+    brand: 'dorothyperkins.com',
+    locale: 'UK',
+    language: 'EN',
+  },
+
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  defaultCommandTimeout: 10000,
+  chromeWebSecurity: false,
+
+  e2e: {
+    setupNodeEvents (on, config) {
+
+      // Implement node event listeners here
+    },
+    numTestsKeptInMemory: 0
+  },
+});

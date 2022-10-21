4e2c4049fe035e7ba5515b09ca1f0b3523ff9c20
Jelena236 <milosevicjelena236@gmail.com>
2022-10-21 14:40:18 +0200

    lint issues

--- cypress/e2e/criticalPath/productListingPage.cy.ts
+++ cypress/e2e/criticalPath/productListingPage.cy.ts
@@ -56,30 +56,36 @@ describe('Product Listing Page tests', function () {
       plpPage.assertions.assertProductVariantIsApplied(productVariations.productAccessories[variables.language]);
     });
     it.skip('Verify size refinement is applied', () => {
+
       // PlpPage.click.sizeRefinement();
       plpPage.click.selectRefinementVariantSize(productVariations.Size[variables.locale]);
       plpPage.assertions.assertProductVariantIsApplied(productVariations.Size[variables.locale]);
     });
     it.skip('Verify style refinement is applied', () => {
+
       // PlpPage.click.styleRefinement();
       plpPage.click.selectRefinementVariantStyle(productVariations.productShopByStyle[variables.language]);
       plpPage.assertions.assertProductVariantIsApplied(productVariations.productShopByStyle[variables.language]);
     });
     it.skip('Verify colour refinement is applied', () => {
+
       // PlpPage.click.colorRefinement();
       plpPage.click.selectRefinementVariantColour(productVariations.ColorBlack[variables.language]);
       plpPage.assertions.assertProductVariantIsApplied(productVariations.ColorBlack[variables.language]);
     });
     it.skip('Verify price refinement is applied', () => {
+
       // PlpPage.click.priceRefinements();
       plpPage.click.selectRefinementVariantShopByPrice(productVariations.priceRangePLPrefinements.range0to5); 
     });
     it.skip('Verify shop by fit refinement is applied', () => {
+
       // PlpPage.click.shopByFitRefinements();
       plpPage.click.selectRefinementVariantShopByFit(productVariations.productShopByFitRefinementTall[variables.language]);
       plpPage.assertions.assertProductVariantIsApplied(productVariations.productShopByFitRefinementTall[variables.language]);
     });
     it.skip('Verify occasion refinement is applied', () => {
+
       // PlpPage.click.occassionRefinement();  
       plpPage.click.selectRefinementVariantOccassion(productVariations.productShopByOccassionRefinementCasual[variables.language]);
       plpPage.assertions.assertProductVariantIsApplied(productVariations.productShopByOccassionRefinementCasual[variables.language]);   

8ca63b4c148925739a71a321ebbcf40431d3e690
CristianIonescu1 <cristian-valentin.ionescu@endava.com>
2022-11-08 12:40:43 +0200

    BoohooMan orderCreation

---
+++ config/boohooman/cypress.boohooman.uk.dev.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'cypress';
+import plugins from '../../cypress/plugins';
+
+export default defineConfig({
+  projectId: 'i6d3n8',
+
+  env: {
+    url: 'https://storefront:Oreo2022@dwdev.boohooman.com/', 
+    sku: 'MZZ61184',
+    fullSKU: 'MZZ61184-131-30',
+    brand: 'boohooman.com',
+    locale: 'UK',
+    language: 'EN',
+  },
+
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  defaultCommandTimeout: 10000,
+  chromeWebSecurity: false,
+  video: false,
+  screenshotOnRunFailure: true,
+
+  e2e: {
+    setupNodeEvents (on) {
+      plugins(on);
+    },
+    numTestsKeptInMemory: 0
+  },
+});

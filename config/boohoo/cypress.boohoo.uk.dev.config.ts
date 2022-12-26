e5e277036e9aa972f7c9a8a773a1185b5b061834
Ivan Vukovic <ivan.vukovic@endava.com>
2022-12-26 18:17:32 +0100

    Supress Bluetooth warning on TeamCity.

--- config/boohoo/cypress.boohoo.uk.dev.config.ts
+++ config/boohoo/cypress.boohoo.uk.dev.config.ts
@@ -23,7 +23,15 @@ export default defineConfig({
   e2e: {
     experimentalSessionAndOrigin: true,
     setupNodeEvents (on) {
-      plugins(on);
+      plugins(on),
+
+      on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.family === 'chromium') {
+          launchOptions.args.push("excludeSwitches: [enable-automation]");
+          launchOptions.args.push("excludeSwitches: [enable-logging]");
+        }
+      return launchOptions
+    })
     },
 
     numTestsKeptInMemory: 0

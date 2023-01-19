bed19aae4e90bb3ba2ed09243b5ea73a02566fae
miona.jovicevic <mionaboohoo@gmail.com>
2023-01-19 10:11:15 +0100

    refactored shipping page for guests

---
+++ config/karenmillen/cypress.karenmillen.ie.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'cypress';
+import plugins from '../../cypress/plugins';
+
+export default defineConfig({
+  projectId: 'i6d3n8',
+
+  env: {
+    url: 'https://storefront:Oreo2022@stg.karenmillen.com/ie?locale=en_IE', 
+    sku: 'AKK99751-152',
+    fullSKU: 'AKK99751-152-22',
+    brand: 'karenmillen.com',
+    locale: 'UK',
+    language: 'EN'
+  },
+
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  defaultCommandTimeout: 10000,
+  chromeWebSecurity: false,
+  video: false,
+  screenshotOnRunFailure: true,
+  
+  e2e: {
+    experimentalSessionAndOrigin: true,
+    setupNodeEvents (on) {
+      plugins(on);
+    },
+
+    numTestsKeptInMemory: 0
+  },
+
+});
+

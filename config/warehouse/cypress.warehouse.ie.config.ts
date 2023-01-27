041cc2415d65d90d92e563ce412b066d106f18b4
miona.jovicevic <mionaboohoo@gmail.com>
2023-01-27 09:00:07 +0100

    oasis and wh changes

---
+++ config/warehouse/cypress.warehouse.ie.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'cypress';
+import plugins from '../../cypress/plugins';
+
+export default defineConfig({
+  projectId: 'i6d3n8',
+
+  env: {
+    url: 'https://storefront:Oreo2022@dwstg.warehousefashion.com/ie',
+    sku: 'BWW04329',
+    brand: 'warehousefashion.com',
+    locale: 'UK',
+    language: 'EN'
+  },
+
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  defaultCommandTimeout: 10000,
+  chromeWebSecurity: false,
+  video: false,
+  screenshotOnRunFailure: true,
+  
+  e2e: {
+    setupNodeEvents (on) {
+      plugins(on);
+    },
+    excludeSpecPattern: [
+      '**/backend*/**' // Skip backend tests
+    ],
+    experimentalSessionAndOrigin: true,
+    numTestsKeptInMemory: 0,
+  },
+
+});
+

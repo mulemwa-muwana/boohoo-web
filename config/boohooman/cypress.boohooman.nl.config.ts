27ecaa9eed4294d246d443194aaa633fbe63e8bd
miona.jovicevic <mionaboohoo@gmail.com>
2023-01-31 14:50:42 +0100

    removed duplated tests from shipping page for guests

---
+++ config/boohooman/cypress.boohooman.nl.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'cypress';
+import plugins from '../../cypress/plugins';
+
+export default defineConfig({
+  projectId: 'i6d3n8',
+
+  env: {
+    url: 'https://storefront:Oreo2022@dwstg.boohooman.com/nl',
+    sku: 'BMM26351-105',
+    brand: 'boohooman.com',
+    locale: 'NL',
+    language: 'NL',
+  },
+
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  defaultCommandTimeout: 10000,
+  chromeWebSecurity: false,
+  video: false,
+  screenshotOnRunFailure: true,
+
+  e2e: {
+    setupNodeEvents (on) {
+      plugins(on);
+    },
+    excludeSpecPattern: [
+      '**/backend*/**' // Skip backend tests
+    ],
+    experimentalSessionAndOrigin: true,
+    numTestsKeptInMemory: 0,
+  },
+});

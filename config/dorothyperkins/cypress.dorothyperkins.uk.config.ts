13d86e0fd894937b0e0c716b23501ad7b297334b
Muneeb Akhtar <muneeb.akhtar@boohoo.com>
2023-04-11 14:07:29 +0100

    fixed defaulttimeout from 60000 to 10000ms dorothyperkins brand

--- config/dorothyperkins/cypress.dorothyperkins.uk.config.ts
+++ config/dorothyperkins/cypress.dorothyperkins.uk.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
 
   viewportHeight: 1080,
   viewportWidth: 1920,
-  defaultCommandTimeout: 60000,
+  defaultCommandTimeout: 10000,
   chromeWebSecurity: false,
   video: false,
   screenshotOnRunFailure: true,

b6feb3f5b943dcbd1e7e1f2084af31221081984b
Miona Jovicevic <Miona.Jovicevic@endava.com>
2023-05-09 14:16:09 +0200

    changes for NG mobiles

--- cypress/pom/plp.page.ts
+++ cypress/pom/plp.page.ts
@@ -52,7 +52,7 @@ const selectors: SelectorBrandMap = {
     loadMoreProducts: 'div.b-load_more > a',
     loadMoreProductsMobile: '.b-load_more-button',
     numberOfItemsTextIsVisible: 'div.b-load_progress > span',
-    productColorIsDisplayedOnPLP: '.b-product_tile_swatches-swatch_image',
+    productColorIsDisplayedOnPLP: '.b-product_tile-container > .b-quickbuy-swatches > .b-product_slider-track > .b-product_tile_swatches-swatch_wrapper > .b-product_tile_swatches-swatch',
     newProductPriceIsDispayed: '.m-new',
     productPriceIsDispayed: '.b-price-item',
     productImageIsDisplayed: '#product-grid > div.l-plp_grid > section:nth-child(3) > div.b-product_tile-container > div:nth-child(1) > div.b-product_tile-top > a > picture > img',

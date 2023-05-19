895ac8a6bcd9e6c4e860a26f07465c624fb4e7c3
stefanciric45 <stefan.ciric@endava.com>
2023-05-19 12:09:33 +0200

    new config files for dev/stage/dev_bavkend and new scripts in package.json

---
+++ config/boohoomena/dev/cypress.boohoomena.sa.dev.config.ts
@@ -0,0 +1,42 @@
+import { defineConfig } from 'cypress';
+import plugins from '../../../cypress/plugins';
+
+export default defineConfig({
+  projectId: 'i6d3n8',
+
+  env: {
+    url: 'https://storefront:Oreo2022@devmena.boohoo.com/en',
+    sku: 'AZZ00912',
+    fullSku: 'AZZ00912-148-20',
+    brand: 'boohoomena.com',
+    locale: 'SA',
+    language: 'EN', 
+  },
+
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  defaultCommandTimeout: 10000,
+  chromeWebSecurity: false,
+  video: false,
+  screenshotOnRunFailure: true,
+
+  // Create Mochawesome report after 'cypress run' tests finish
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'config/boohoomena/results',
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+
+  e2e: {
+    setupNodeEvents (on) {
+      plugins(on);
+    },
+    excludeSpecPattern: [
+      '**/backend*/**' // Skip backend tests
+    ],
+    experimentalSessionAndOrigin: true,
+    numTestsKeptInMemory: 0,
+  },
+});
---
+++ config/wallis/stage/cypress.wallis.uk.config.ts
@@ -0,0 +1,42 @@
+import { defineConfig } from 'cypress';
+import plugins from '../../../cypress/plugins';
+
+export default defineConfig({
+  projectId: 'i6d3n8',
+
+  env: {
+    url: 'https://storefront:Oreo2022@dwstg.wallis.co.uk',
+    sku: '#BYY07099', // Needs hashtag for assertation on PDP
+    fullSku: 'BYY07099-808-24',
+    brand: 'wallis.co.uk',
+    locale: 'UK',
+    language: 'EN',
+  },
+
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  defaultCommandTimeout: 10000,
+  chromeWebSecurity: false,
+  video: false,
+  screenshotOnRunFailure: true,
+
+  // Create Mochawesome report after 'cypress run' tests finish
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'config/wallis/results',
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+
+  e2e: {
+    setupNodeEvents (on) {
+      plugins(on);
+    },
+    excludeSpecPattern: [
+      '**/backend*/**' // Skip backend tests
+    ],
+    experimentalSessionAndOrigin: true,
+    numTestsKeptInMemory: 0,
+  },
+});

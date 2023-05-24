ef291ba79378a7a0a1d99e6a6b4c8e9539a55977
Oyindasola Oladimeji <oyindasola.oladimeji@boohoo.com>
2023-05-24 12:13:49 +0100

    Fix failed test for NG mobile

--- cypress/pom/contactus.page.ts
+++ cypress/pom/contactus.page.ts
@@ -101,11 +101,16 @@ class ContactUsPage implements AbstractPage {
   actions = { };
   assertions = {
     assertTwitterIconIsNotPresent () {
+      const viewportWidth = Cypress.config('viewportWidth');
       const twitterIcon = selectors[variables.brand].twitterIcon;
+      
+      // For either mobile or desktop view
+      if (viewportWidth < 1100 || viewportWidth < 1100 ) {
       if (variables.brand !== 'dorothyperkins.com') {
         cy.scrollTo('top');
       }
       cy.get(twitterIcon).should('not.exist');
+    }
     },
     assertFacebookIconIsPresent () {
       const facebookIcon = selectors[variables.brand].facebookIcon;

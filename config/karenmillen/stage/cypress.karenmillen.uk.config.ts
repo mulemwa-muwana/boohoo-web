1c22f606b4369c49c7bb8af1859e5e631276dda5
Muneeb Akhtar <muneeb.akhtar@boohoo.com>
2023-06-06 18:59:30 +0100

    Resolved Merge Conflicts

--- config/karenmillen/stage/cypress.karenmillen.uk.config.ts
+++ config/karenmillen/stage/cypress.karenmillen.uk.config.ts
@@ -19,7 +19,6 @@ export default defineConfig({
   chromeWebSecurity: false,
   video: false,
   screenshotOnRunFailure: true,
-  numTestsKeptInMemory: 150,
 
   // Create Mochawesome report after 'cypress run' tests finish
   reporter: 'mochawesome',
@@ -38,8 +37,7 @@ export default defineConfig({
       '**/backend*/**' // Skip backend tests
     ],
     experimentalSessionAndOrigin: true,
-
-    // NumTestsKeptInMemory: 0,
+    numTestsKeptInMemory: 0,
   },
 
 });

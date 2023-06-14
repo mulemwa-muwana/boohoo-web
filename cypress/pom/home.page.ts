ecdf88f5a1e87cf64171f2264eea40ea84dbcd8e
Parita Patel <parita.patel@boohoo.com>
2023-06-14 10:58:29 +0100

    fixed Search resultpage assertion issue

--- cypress/pom/home.page.ts
+++ cypress/pom/home.page.ts
@@ -406,7 +406,7 @@ class HomePage implements AbstractPage {
     },
     assertSearchResultPage (text: string) {
       const helper = text.split('-');
-      const finalSku = helper[0].trim().replace('#','');
+      const finalSku = helper[0].trim().replaceAll('#','');
       cy.url().should('include', finalSku);
     },
     assertAutosearchSuggestionsDispayed () {

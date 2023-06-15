285b8fa385e2864d42befb4c22106a9f5f32e7cc
Ivan Vukovic <ivan.vukovic@endava.com>
2023-06-15 09:24:02 +0200

    Remove duplicate method in e2e.ts

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -9,13 +9,6 @@ Cypress.on('uncaught:exception', () => {
   return false;
 });
 
-//To turn off all uncaught exception handling
-Cypress.on('uncaught:exception', (err, runnable) => {
-  // returning false here prevents Cypress from
-  // failing the test
-  return false
-})
-
 const variables = Cypress.env() as EnvironmentVariables;
 export const brand = variables.brand;
 export const locale = variables.locale;

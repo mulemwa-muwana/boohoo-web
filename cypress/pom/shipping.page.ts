226e29922fef7e5f429e390fd9a54ff21ea33bd2
Burak Tuna <burak.tuna@boohoo.com>
2023-06-16 11:33:05 +0100

    addPremierToCartFromShippingPage locater is changed.

--- cypress/pom/shipping.page.ts
+++ cypress/pom/shipping.page.ts
@@ -534,7 +534,7 @@ const selectors: SelectorBrandMap = {
   'misspap.com': {
     promoCodeBtn: 'button[data-tau="coupon_submit"]',
     PUDOlocations: 'a.delivery-tabs-link:nth-child(2)',
-    addPremierToCartFromShippingPage: '.premier-box-btn.js-premier-box-link',
+    addPremierToCartFromShippingPage: '#add-to-cart',
     addPremierToCartFromShippingPageMobile: '.premier-box-btn.js-premier-box-link',
     addPremierToBagMobile: '#add-to-cart',
     premierProductTitle: 'VIP DELIVERY',

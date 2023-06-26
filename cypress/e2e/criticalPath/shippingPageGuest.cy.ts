b357e6ddc62f2a95cdf1d3b7fc1f06cd3d34e04d
Parita Patel <parita.patel@boohoo.com>
2023-06-26 12:15:06 +0100

    added function to select state value in shippingPageGuest.cy.ts

--- cypress/e2e/criticalPath/shippingPageGuest.cy.ts
+++ cypress/e2e/criticalPath/shippingPageGuest.cy.ts
@@ -93,7 +93,7 @@ describe('Shipping Page Guest user tests', function () {
     shippingPage.assertions.assertManualAddressFieldsAreDisplayed();
   });
 
-  it('Verify that user is able to add address details manually', function () {
+  it.only('Verify that user is able to add address details manually', function () {
     const localeAddress = Addresses.getAddressByLocale(locale,'primaryAddress');
     shippingPage.click.addNewAddress();
     shippingPage.click.addAddressManually();
@@ -104,6 +104,9 @@ describe('Shipping Page Guest user tests', function () {
     shippingPage.click.enterManuallyAddressDetails();
     shippingPage.actions.adressLine1(localeAddress.addressLine);
     shippingPage.actions.cityField(localeAddress.city);
+    if (!isSiteGenesisBrand && (locale == 'US')){
+      cy.get('#dwfrm_shipping_shippingAddress_addressFields_states_stateCode').select(1)
+    }
     shippingPage.actions.postcodeField(localeAddress.postcode);
     shippingPage.actions.phoneNumberField(localeAddress.phone);
     if (isSiteGenesisBrand) {

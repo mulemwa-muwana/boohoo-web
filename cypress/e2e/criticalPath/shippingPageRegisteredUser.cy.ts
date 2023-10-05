844607038b630b36c1a92c7dd1d8ab690691d4ad
Parita Patel <parita.patel@boohoo.com>
2023-10-05 10:18:31 +0100

    Resolved shippingpage issue

--- cypress/e2e/criticalPath/shippingPageRegisteredUser.cy.ts
+++ cypress/e2e/criticalPath/shippingPageRegisteredUser.cy.ts
@@ -303,8 +303,8 @@ describe('Shipping Page Registered user tests', function () {
   });
 
   it('Verify that user can enter valid credentials in w3w', function () {
-    const excludedmisspapWithLocales: boolean = brand == 'misspap.com' || brand == 'nastygal.com'|| brand == 'boohoo.com'&& (locale == 'IE' || locale == 'AU' || locale == 'US');
-    if (brand == 'boohooman.com' || brand == 'boohoomena.com' || excludedmisspapWithLocales) {
+    const excludedmisspapWithLocales: boolean = ((brand == 'misspap.com' || brand == 'nastygal.com'|| brand == 'boohoo.com')&& (locale == 'IE' || locale == 'AU' || locale == 'US')) || (brand == 'boohooman.com' || brand == 'boohoomena.com' );
+    if (excludedmisspapWithLocales) {
       this.skip();
     }
     const localeAddress = Addresses.getAddressByLocale(locale, 'primaryAddress');

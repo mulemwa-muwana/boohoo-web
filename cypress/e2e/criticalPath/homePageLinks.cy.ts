9c79cfc10d17bf251b91e96e325cc3ae6fee85fd
chirag.patel <chirag.patel@boohoo.com>
2023-12-06 13:59:47 +0000

    lint fix

--- cypress/e2e/criticalPath/homePageLinks.cy.ts
+++ cypress/e2e/criticalPath/homePageLinks.cy.ts
@@ -1,25 +1,24 @@
 import { brand } from 'cypress/support/e2e';
 import HomePage from '../../pom/home.page';
 
-
 describe('Home Page', function () {
 
-    beforeEach(() => {
-        HomePage.goto();
+  beforeEach(() => {
+    HomePage.goto();
+  });
+
+  // Iterate the Links
+  if (brand == 'boohooman.com') {
+    describe('Strategic Homepage Links Tests', () => {
+      it('Iterate thorugh sifu-header meganav links on homepage and validate Page status is OK', () => {
+        HomePage.assertions.assertAllHeaderMegaNavLinksStatus();
+      });
+
+      it('Iterate thorugh sifu-footer links on homepage and validate Page status is OK', () => {
+        HomePage.assertions.assertAllFooterLinksStatus();
+      });
+
     });
-
-    // Iterate the Links
-    if (brand == 'boohooman.com') {
-        describe('Strategic Homepage Links Tests', () => {
-            it('Iterate thorugh sifu-header meganav links on homepage and validate Page status is OK', () => {
-                HomePage.assertions.assertAllHeaderMegaNavLinksStatus();
-            });
-
-            it('Iterate thorugh sifu-footer links on homepage and validate Page status is OK', () => {
-                HomePage.assertions.assertAllFooterLinksStatus();
-            });
-
-        });
-    }
+  }
 
 });

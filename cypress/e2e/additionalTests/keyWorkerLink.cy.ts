7f39038a26ccd0fb74f28a9a9b5c953797cfe927
Ashwini Pally <Ashwini.Pally@boohoo.com>
2023-12-08 10:59:29 +0000

    Adding brand/locale before describe

--- cypress/e2e/additionalTests/keyWorkerLink.cy.ts
+++ cypress/e2e/additionalTests/keyWorkerLink.cy.ts
@@ -2,52 +2,41 @@ import { brand, locale, language } from 'cypress/support/e2e';
 import assertionText from '../../helpers/assertionText';
 import keyWorkerPage from 'cypress/pom/keyWorker.page';
 import navigate from 'cypress/helpers/navigate';
-​
-describe('KeyWorkerDiscount Tests', () => {
-​
-  // This will execute before every single test, we're just going to the baseURL.
-  beforeEach(() => {
-    navigate.keyWorkerPage();
-  });
-  it('Verify that key worker link opens', function () {
-    if (brand == 'boohooman.com' && locale == 'UK') {
+
+if (brand == 'boohooman.com' && locale == 'UK') {
+
+  describe('KeyWorkerDiscount Tests', () => {
+
+    // This will execute before every single test, we're just going to the baseURL.
+    beforeEach(() => {
+      navigate.keyWorkerPage();
+    });
+    it('Verify that key worker link opens', function () {
+
       keyWorkerPage.assertions.assertKeyWorkerFormIsPresent(assertionText.KeyWorkerForm[language])
-    }
-    else {
-      this.skip
-    }
-  })
-​
-  it('Verify that key worker link opens & user can start a subscription', function () {
-    if (brand == 'boohooman.com' && locale == 'UK') {
+    })
+
+    it('Verify that key worker link opens & user can start a subscription', function () {
       keyWorkerPage.assertions.assertKeyWorkerFormIsPresent(assertionText.KeyWorkerForm[language]);
       cy.fixture('newuser').then((credentials) => {
         keyWorkerPage.actions.EnterData(credentials.firstname, credentials.lastname);
         keyWorkerPage.actions.EnterEmail(credentials.username);
         keyWorkerPage.actions.chooseDate('24', assertionText.DOBmonth[language], '2000');
         keyWorkerPage.click.signupButton();
-​
+
       }
       )
-    }
-    else {
-      this.skip
-    }
-​
-  })
-​
-  it('Verify that key worker link opens & user can not Subscribe with empty data ', function () {
-    if (brand == 'boohooman.com' && locale == 'UK') {
+
+    })
+
+    it('Verify that key worker link opens & user can not Subscribe with empty data ', function () {
       cy.fixture('newuser').then((credentials) => {
         keyWorkerPage.actions.EnterData(credentials.firstname, credentials.lastname);
         keyWorkerPage.actions.chooseDate('19', assertionText.DOBmonth[language], '2000');
         keyWorkerPage.click.signupButton();
       }
       )
-    }
-    else {
-      this.skip
-    }
-​
+
+    })
   })
-});
+};
